8bc356ae8847e79b263d5982dd0df2965a15d667
cesaralvarod <cesaralvarod@gmail.com>
2023-09-04 01:30:57 -0500

    7 and 8 operators

---
+++ 7.arithmetic_operators/operators.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // aithmetic operators
+
+    // + (addition)
+    // - (substraction)
+    // * (multiplication)
+    // / (division)
+    // % (modulus)
+    // ++ (increment)
+    // -- (decrement)
+
+    int x = 5;
+    int y = 2;
+
+    printf("%d", x + y);
+}
---
+++ 8. augmented_assignment_operators/operators.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    // used to replcae a statement where an operator takes a variable as one of
+    // this arguments and then assigns the result back to the same variable
+
+    int x = 10;
+
+    x += 2;
+
+    printf("%d", x);
+
+    return 0;
+}
